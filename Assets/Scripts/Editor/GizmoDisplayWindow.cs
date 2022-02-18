6406f960f915e6386ccc81016bcf094c4836ca2d
aFranchon <3032fmartinage@gmail.com>
2022-02-19 00:52:57 +0100

    Add Gizmos are slectable and movable

--- Assets/Scripts/Editor/GizmoDisplayWindow.cs
+++ Assets/Scripts/Editor/GizmoDisplayWindow.cs
@@ -1,5 +1,6 @@
 ﻿#if UNITY_EDITOR
 
+using JetBrains.Annotations;
 using technical.test.editor;
 using UnityEditor;
 using UnityEngine;
@@ -13,6 +14,9 @@ namespace Editor
         private string _path = "Assets/Data/Editor/Scene Gizmo Asset.asset";
 
         public static SceneGizmoAsset GizmoAsset => _gizmoAsset;
+
+        private static GameObject _selectedGameObject;
+        private static int _selectGizmoIndex = -1;
         
         [MenuItem("Window/Custom/Show Gizmos")]
         public static void Initialize()
@@ -21,43 +25,106 @@ namespace Editor
             window.Show();
         }
 
+        private void Awake()
+        {
+            Debug.Log("here");
+            AssemblyReloadEvents.beforeAssemblyReload += () =>
+            {
+                if (_selectedGameObject != null) DestroyImmediate(_selectedGameObject);
+            };
+        }
+
+        private void Update()
+        {
+            if (_selectGizmoIndex < 0) return;
+
+            _gizmoAsset.Gizmos[_selectGizmoIndex].Position = _selectedGameObject.transform.position;
+        }
+
         //TODO call this from SceneGizmoAsset
         public static void Initialize(SceneGizmoAsset gizmoAsset)
         {
             _gizmoAsset = gizmoAsset == null ? _gizmoAsset : gizmoAsset;
             Initialize();
         }
-
-        //TODO load gizmo after compilation
         
         private void OnGUI()
         {
             GUILayout.Label("Gizmo Editor", EditorStyles.boldLabel);
             
+            DisplayGizmoAssetInfos();
+            DisplayGizmosInfos();
+        }
+
+        private void DisplayGizmoAssetInfos()
+        {
             EditorGUILayout.TextField("SceneGizmoAsset asset path", _path);
             if (GUILayout.Button("Load SceneGizmoAsset"))
             {
                 _gizmoAsset = Utils.GetSceneGizmoAssetFromPath(_path);
             }
 
-            if (_gizmoAsset == null) return;
+            if (_gizmoAsset == null)
+            {
+                _gizmoAsset = Utils.GetSceneGizmoAssetFromPath(_path);
+            }
+        }
 
+        private void DisplayGizmosInfos()
+        {
             for (var i = 0; i < _gizmoAsset.Gizmos.Length; i++)
             {
                 var gizmo = _gizmoAsset.Gizmos[i];
                 
                 GUILayout.BeginHorizontal();
-                //TODO Add save here
+                
+                GUI.color = i == _selectGizmoIndex ? Color.red : Color.gray;
+                GUI.backgroundColor = i == _selectGizmoIndex ? Color.red : Color.white;
+                
                 gizmo.Name = EditorGUILayout.TextField("Name", gizmo.Name);
                 gizmo.Position = EditorGUILayout.Vector3Field("position", gizmo.Position);
+                
                 if (GUILayout.Button("Edit"))
                 {
-                    //TODO Edit selected gizmo
+                    if (i == _selectGizmoIndex)
+                    {
+                        UnselectGizmo();
+                    }
+                    else
+                    {
+                        SelectGizmo(gizmo, i);
+                    }
                 }
 
                 _gizmoAsset.Gizmos[i] = gizmo;
                 GUILayout.EndHorizontal();
             }
+        }
+
+        private void SelectGizmo(Gizmo gizmo, int index)
+        {
+            Tools.current = Tool.Move;
+                    
+            if (_selectedGameObject != null)
+            {
+                DestroyImmediate(_selectedGameObject);
+            }
+
+            _selectedGameObject = new GameObject("GizmoPosition => " + gizmo.Name);
+            _selectedGameObject.transform.position = gizmo.Position;
+                    
+            Selection.activeGameObject = _selectedGameObject;
+            _selectGizmoIndex = index;
+        }
+
+        private void UnselectGizmo()
+        {
+            if (_selectedGameObject != null)
+            {
+                DestroyImmediate(_selectedGameObject);
+            }
+            
+            _selectGizmoIndex = -1;
         }
         
         [DrawGizmo(GizmoType.Active | GizmoType.Selected | GizmoType.NonSelected)]
